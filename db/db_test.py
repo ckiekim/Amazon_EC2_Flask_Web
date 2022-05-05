3fd2500a9ba83e30ff5add06193cc1274654d838
Cheolkie Kim <ckiekim@naver.com>
2022-05-05 23:46:22 +0000

    MariaDB

--- db/db_test.py
+++ db/db_test.py
@@ -9,15 +9,15 @@ def get_bbs_list(offset=0):
     conn = mc.connect(**config)
     cur = conn.cursor()
     sql = '''SELECT b.bid, b.uid, u.uname, b.title, b.content, 
-                    DATE_FORMAT(b.modTime, '%%Y-%%m-%%d %%H:%%i:%%s') AS modTime,
+                    DATE_FORMAT(b.modTime, '%Y-%m-%d %T') AS modTime,
                     b.viewCount, b.replyCount
                 FROM bbs AS b
                 JOIN users AS u
                 ON b.uid=u.uid
                 WHERE b.isDeleted=0
                 ORDER BY b.bid DESC 
-                LIMIT 10 offset 0;'''
-    cur.execute(sql)    #, (offset,))
+                LIMIT 10 offset %s;'''
+    cur.execute(sql, (offset,))
     rows = cur.fetchall()
     cur.close()
     conn.close()
