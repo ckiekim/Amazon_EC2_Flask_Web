cee0dd8d268090654fb201ea1aca2a5f794044b0
Cheolkie Kim <ckiekim@naver.com>
2022-05-05 22:05:44 +0900

    MariaDB

--- db/db_init.py
+++ db/db_init.py
@@ -15,7 +15,7 @@ sql = '''
         pwd CHAR(44) NOT NULL, 
         uname VARCHAR(20) DEFAULT 'Guest', 
         email VARCHAR(40) NOT NULL,
-        reg_date DATETIME DEFAULT CURRENT_TIMESTAMP, 
+        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
         is_deleted INT DEFAULT 0 
     );
 '''
@@ -38,7 +38,7 @@ sql = '''
         uid VARCHAR(20) NOT NULL,
         title VARCHAR(100) NOT NULL,
         content VARCHAR(1000),
-        modTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+        modTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         viewCount INT DEFAULT 0,
         replyCount INT DEFAULT 0,
         isDeleted INT DEFAULT 0,
@@ -64,7 +64,7 @@ sql = '''
         bid INT NOT NULL,
         uid VARCHAR(20) NOT NULL,
         content VARCHAR(100),
-        regTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+        regTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         isMine INT DEFAULT 0,
         FOREIGN KEY(bid) REFERENCES bbs(bid),
         FOREIGN KEY(uid) REFERENCES users(uid)
