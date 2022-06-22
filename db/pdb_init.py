26ae747f1c8f8ff72e8897e61b1efd27e27a93d2
Cheolkie Kim <ckiekim@naver.com>
2022-06-22 17:22:11 +0900

    프로젝트 게시판

--- db/pdb_init.py
+++ db/pdb_init.py
@@ -21,10 +21,31 @@ sql = '''
         files VARCHAR(100),
         vc INT DEFAULT 0,
         ht VARCHAR(100),
-        isDeleted INT DEFAULT 0,
+        isDeleted INT DEFAULT 0
     ) AUTO_INCREMENT=101;
 '''
 cur.execute(sql)
+
+authors = [
+        {'name':'제임스', 'email':'james@ckworld.com'},
+        {'name':'마리아', 'email':'maria@ckworld.com'}
+    ]
+files = ['가나다.pdf', 'abc.mp4']
+title = '손그림 색칠 플라스크 구현'
+content = '손그림 색칠 플라스크 구현 프로젝트'
+term = '2022.06'
+vc = 3
+cn = '지능형 빅데이터 서비스 개발 과정(9,10회차)'
+co = '멀티캠퍼스'
+ht = ['#인공지능','#GAN','#파이썬','#플라스크','#웹']
+
+sql = '''
+    INSERT INTO pbbs(title,content,cn,co,authors,term,files,vc,ht)
+    VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s);
+'''
+params = (title,content,cn,co,json.dumps(authors),term,json.dumps(files),vc,json.dumps(ht))
+cur.execute(sql, params)
+conn.commit()
 
 cur.close()
 conn.close()
